8319963a9957dc5abea6a6a2c0518beab847d8ea
Jamar Parham <Jamar720@gmail.com>
2018-04-30 21:42:31 -0400

    added

---
+++ app/data/friends.js
@@ -0,0 +1,129 @@
+
+var friendsArray = [
+
+    {
+        "name": "Alfred",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/10915224_10205856555481343_1423772962318534158_n.jpg?_nc_cat=0&oh=d1f874ab48097941f8973fb7cf9ad4aa&oe=5B523260",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+
+    {
+        "name": "Colby",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/21752069_10210496992681052_3616259168716989782_n.jpg?_nc_cat=0&oh=89403a0c6bb5b30ba9af93f0008b0050&oe=5B5745D6",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            4
+        ]
+    },
+
+    {
+        "name": "Reid",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/405210_2866625196124_1974997771_n.jpg?_nc_cat=0&oh=303a3fb8aacc2bf1291d0f9e7a88ec71&oe=5B639E50",
+        "scores": [
+            4,
+            5,
+            5,
+            3,
+            1
+        ]
+    },
+
+    {
+        "name": "Craig",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/20156092_1613568261987176_2202749640595149778_n.jpg?_nc_cat=0&oh=8848c86c7be6a0dd1659ed4ecc207eaf&oe=5B9720BC",
+        "scores": [
+            5,
+            5,
+            5,
+            1,
+            1
+        ]
+    },
+
+    {
+        "name": "Jamar",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/24993416_1927948437232694_990868106635849712_n.jpg?_nc_cat=0&oh=8d7a1e1fec4d8f77d2ca2c88a9287a53&oe=5B619947",
+        "scores": [
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+
+    {
+        "name": "Desmond",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/543341_3342337042842_2087404287_n.jpg?_nc_cat=0&oh=74674946450fd02446910b12c73a90ae&oe=5B5D1520",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+
+    {
+        "name": "kris",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/425460_2571689623457_1910465520_n.jpg?_nc_cat=0&oh=d0feaebc0146ee322fc5b448d517bae4&oe=5B56AC0F",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+
+    {
+        "name": "dorian",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/22308874_10156462013563783_1862691530676836317_n.jpg?_nc_cat=0&oh=85eb190cf41cb09bd3f9cc0fda7c354f&oe=5B5D4D7B",
+        "scores": [
+            1,
+            1,
+            0,
+            1,
+            1
+        ]
+    },
+
+    {
+        "name": "Karl",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/29792849_10155393176490334_1382931620120420464_n.jpg?_nc_cat=0&oh=c81b5c4f03bf6b35a27c2f56e3958ea1&oe=5B5D5DC3",
+        "scores": [
+            1,
+            0,
+            1,
+            1,
+            1
+        ]
+    },
+
+    {
+        "name": "tommy",
+        "photo": "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/30656405_10211135279568951_2392796348304275981_n.jpg?_nc_cat=0&oh=78ed829fd1e54917bbcd99e0df8199c5&oe=5B9A8B97",
+        "scores": [
+            3,
+            6,
+            1,
+            1,
+            1
+        ]
+    },
+],
+
+
+
+
+module.exports = friendsArray;
+
